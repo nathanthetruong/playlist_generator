b64da41a13c6b2857b9a2efa747a23bab3a5a71c
nathan <t.nathan235@gmail.com>
2024-09-30 23:06:35 -0500

    Added database table creation script

--- api_key_handling.py
+++ api_key_handling.py
@@ -4,5 +4,6 @@ from dotenv import load_dotenv
 # Loads .env file
 load_dotenv()
 
-# Stores all the API keys
+# Stores all the keys and credentials
+PSQL_PASSWORD = os.getenv('PSQL_PASSWORD')
 STEAM_KEY = os.getenv('STEAM_API_KEY')
---
+++ database_table_creation.py
@@ -0,0 +1,27 @@
+import psycopg2
+from api_key_handling import PSQL_PASSWORD
+
+connection = psycopg2.connect(database="game_data_storage",
+                        host="localhost",
+                        user="nathan",
+                        password=PSQL_PASSWORD,
+                        port="5432")
+
+cursor = connection.cursor()
+
+# Creates Game database table
+create_table_query = """
+CREATE TABLE IF NOT EXISTS Game (
+    game_id SERIAL PRIMARY KEY,
+    app_id INTEGER NOT NULL,
+    name VARCHAR(255) NOT NULL,
+    description TEXT NOT NULL
+    );
+"""
+
+cursor.execute(create_table_query)
+
+# Commits changes and closes the database connection
+connection.commit()
+cursor.close()
+connection.close()
